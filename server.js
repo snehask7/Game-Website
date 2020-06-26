bba14b0e0561b9d0622c024f2d036b248936c523
snehask7 <snehaskannan@gmail.com>
2020-06-26 15:25:04 +0530

    procdel

--- server.js
+++ server.js
@@ -2,9 +2,7 @@ const express = require('express');
 const compression = require('compression');
 const path = require('path');
 
-const app = express();
-app.use(compression());
-app.use(express.static(path.join(__dirname, 'build')));
+
 
 app.get('*', (req, res) => {
     res.sendFile(path.join(__dirname, 'build', 'index.html'));
